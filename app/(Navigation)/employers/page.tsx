b8dfd7300e3595897a5bf086e5720aa879405c3e
Abdul-asa <bigtulahi1@gmail.com>
2024-08-24 18:59:05 +0100

    starting employers page

---
+++ app/(Navigation)/employers/page.tsx
@@ -0,0 +1,7 @@
+export default function EmployerPage() {
+  return (
+    <div className="mx-5 mt-4 flex flex-col items-center justify-center gap-7 sm:mx-auto sm:w-3/5">
+      Hello Employers
+    </div>
+  );
+}
--- app/api/route.ts
+++ app/api/route.ts
@@ -1,10 +1,10 @@
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+  // const res = NextResponse.
   return {
     status: 200,
     body: "Hello, world!",
     request,
-    response: NextResponse,
   };
 }
