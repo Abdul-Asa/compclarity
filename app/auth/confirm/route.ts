bf8f3686981a70971ba91a5ac9a90ece82d07fe8
Abdul-asa <bigtulahi1@gmail.com>
2024-08-24 22:25:10 +0100

    API route better fix

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -3,19 +3,15 @@ import { type NextRequest, NextResponse } from "next/server";
 
 import { createClient } from "@/lib/supabase/server";
 
-// Figure out how Amplify handles Next.js api routes
-// Add localhost to the allowed origins in the Supabase settings
 export async function GET(request: NextRequest) {
-  console.log(request);
+  let APP_URL = "https://compclarity.com/";
+  if (process.env.NODE_ENV === "development") {
+    APP_URL = "http://localhost:3000/";
+  }
+
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
-  const next = searchParams.get("next") ?? "/";
-
-  const redirectTo = request.nextUrl.clone();
-  redirectTo.pathname = next;
-  redirectTo.searchParams.delete("token_hash");
-  redirectTo.searchParams.delete("type");
 
   if (token_hash && type) {
     const supabase = createClient();
@@ -25,12 +21,10 @@ export async function GET(request: NextRequest) {
       token_hash,
     });
     if (!error) {
-      redirectTo.searchParams.delete("next");
-      return NextResponse.redirect("https://compclarity.com/login");
+      return NextResponse.redirect(APP_URL + "login");
     }
 
-    redirectTo.pathname = "/error";
-    const response = NextResponse.redirect("https://compclarity.com/404");
+    const response = NextResponse.redirect(APP_URL + "error");
 
     console.log(response);
     return response;
