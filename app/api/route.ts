b0a7a19eef38f1406fc0d09fbaba3577686377cc
Abdul-asa <bigtulahi1@gmail.com>
2024-08-24 19:12:09 +0100

    testing api routes

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,10 +1,26 @@
-import { type NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  // const res = NextResponse.
-  return {
+  const reqUrl = request.nextUrl.toString();
+  const { searchParams, href } = new URL(request.url);
+  const res = new NextResponse();
+
+  const responseBody = {
     status: 200,
     body: "Hello, world!",
-    request,
+    href,
+    req: {
+      url: reqUrl,
+      method: request.method,
+      headers: Object.fromEntries(request.headers.entries()), // Convert headers to a plain object
+    },
+    searchParams: Object.fromEntries(searchParams.entries()), // Convert searchParams to a plain object
+    res: {
+      url: res.url,
+      status: res.status,
+      headers: Object.fromEntries(res.headers.entries()), // Convert headers to a plain object
+    },
   };
+
+  return NextResponse.json(responseBody);
 }
