5f65abf5229af0449f0817f13935bc70be4e43ec
Abdul-asa <bigtulahi1@gmail.com>
2024-08-30 11:59:56 +0100

    surveys

--- components/providers/posthog.tsx
+++ components/providers/posthog.tsx
@@ -2,13 +2,15 @@
 "use client";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
+import { useEffect } from "react";
 
-if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
-    person_profiles: "identified_only",
-  });
-}
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com",
+      capture_pageview: true,
+    });
+  }, []);
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
