3b0c19e145827b6d40c652ef72ddcc349591d870
Abdul-asa <bigtulahi1@gmail.com>
2024-09-12 23:10:59 +0100

    hiding og & twitter desc. for good

--- config/metadata.tsx
+++ config/metadata.tsx
@@ -4,20 +4,20 @@ export const metadata: Metadata = {
   title: "CompClarity",
   description:
     "Your guide to fair pay from day one. Compare salaries across Europe and make informed career choices with comprehensive, community-driven data!",
-  metadataBase: new URL("https://compclarity.com"),
-  twitter: {
-    card: "summary_large_image",
-    title: "CompClarity",
-    // description:
-    //   "Your guide to fair pay from day one. Compare salaries across Europe and make informed career choices with comprehensive, community-driven data!",
-    // images: ["/assets/og.png"],
-  },
-  openGraph: {
-    title: "CompClarity",
-    // description:
-    //   "Your guide to fair pay from day one. Compare salaries across Europe and make informed career choices with comprehensive, community-driven data!",
-    // images: ["/assets/og.png"],
-  },
+  // metadataBase: new URL("https://compclarity.com"),
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "CompClarity",
+  //   description:
+  //     "Your guide to fair pay from day one. Compare salaries across Europe and make informed career choices with comprehensive, community-driven data!",
+  //   images: ["/assets/og.png"],
+  // },
+  // openGraph: {
+  //   title: "CompClarity",
+  //   description:
+  //     "Your guide to fair pay from day one. Compare salaries across Europe and make informed career choices with comprehensive, community-driven data!",
+  //   images: ["/assets/og.png"],
+  // },
   keywords: [
     "CompClarity",
     "Compensation",
