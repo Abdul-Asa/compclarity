e05b577adad927b465a54d870324fdb617e3b804
Abdul-asa <bigtulahi1@gmail.com>
2024-09-15 15:11:34 +0100

    starting products page

---
+++ app/(Navigation)/products/e-book/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "CompClarity - Privacy Policy",
+  // description: "CompClarity's mission is dedicated to demystifying compensation packages across Europe, starting with the tech industry, by ensuring that you have all the information required to understand your worth and make informed decisions.",
+};
+
+export default function EBook() {
+  return (
+    <div className="mx-5 mt-4 flex flex-col items-center justify-center gap-7 text-lg sm:mx-auto sm:w-3/5">E-Book</div>
+  );
+}
