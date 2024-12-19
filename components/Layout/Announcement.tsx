e03dbe5fa3de438fa1104c3f71686de5782dfc3a
Alexander <59412299+bobabooth@users.noreply.github.com>
2024-12-19 14:33:15 +0000

    Update Announcement.tsx

--- components/Layout/Announcement.tsx
+++ components/Layout/Announcement.tsx
@@ -14,15 +14,23 @@ export function Announcement() {
     if (!hasDisplayed) {
       // Set a timeout to display the toast
       const timeoutId = setTimeout(() => {
-        toast("📢 Share your salary!", {
+        toast("📢 2024 Salary Report", {
           closeButton: true,
           classNames: {
             closeButton: "bg-white",
           },
           id: "welcome-toast",
-          duration: 5000,
-          description: "Received an offer this year? Share your salary to help improve transparency for all!",
-          onAutoClose: () => {
+          duration: 10000,
+          description: (
+            <a
+              href="https://www.linkedin.com/posts/compclarity_2024-tech-salaries-activity-7275474808769822721-oJrJ"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "#007bff", textDecoration: "underline" }}
+            >
+              Check it out here
+            </a>
+          ),          onAutoClose: () => {
             sessionStorage.setItem("announcementDisplayed", "true");
           },
           onDismiss: () => {
